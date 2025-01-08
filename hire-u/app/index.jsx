31708cc2637ce0b09cf0f13a6e978559e8a306ad
tranuyn <127413543+tranuyn@users.noreply.github.com>
2025-01-09 05:11:59 +0700

    create Bottom Tabs Navigator

--- hire-u/app/index.jsx
+++ hire-u/app/index.jsx
@@ -1,12 +1,17 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Home from './home'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Job from './job';
 
+const Tab = createBottomTabNavigator();
 const index = () => {
   return (
-    <View>
-      <Text>index</Text>
-    </View>
-  )
-}
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Profile" component={Job} />
+    </Tab.Navigator>
+  );
+};
 
 export default index
